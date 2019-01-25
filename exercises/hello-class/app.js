e94ad78dff7fb1294983c456fe91415792dc10e7
Victoria Sumner <vicsumner@gmail.com>
2019-01-24 17:14:24 -0700

    commit message

---
+++ exercises/hello-class/app.js
@@ -0,0 +1,17 @@
+
+//function expression
+// var sayHello = function () {
+//   console.log("hello class")
+// }
+// sayHello()
+
+// function declaration
+function sayHello(name){
+  console.log("Hello " + name)
+}
+sayHello("Vicki")
+
+
+//argument
+//parameter
+
