27f3893e1d5075a94fa4e399989f713161eb15ee
Victoria Sumner <vicsumner@gmail.com>
2019-01-25 18:49:47 -0700

    commit message

---
+++ exercises/evenOddlooping/app.js
@@ -0,0 +1,32 @@
+
+// -------------- simple for loop to 100-------
+// for(let i=0; i<=100; i++){
+//     console.log(i);
+// }
+
+// -------------- simple even number loop to print only even #s to 101-------
+
+// for (let i = 0; i <= 101; i++) {
+//   if (i % 2 == 0) {
+//     console.log(i);
+//   }
+// }
+
+// -------------- simple odd number loop to print only odd #s to 101-------
+
+// for (let i = 0; i <= 101; i++) {
+//   if (i % 2 !== 0) {
+//     console.log(i);
+//   }
+// }
+
+// -------------- If the current iteration is an Odd number,print "Odd" to the console, otherwise print "Even".-------------------------------------
+
+for (let i = 0; i < 1; i++) {
+  if (100 % 2 === 0) {
+    console.log("Even");
+  } 
+  else {
+    console.log("Odd");
+  }
+};
