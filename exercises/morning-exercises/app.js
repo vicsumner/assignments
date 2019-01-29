672c259d8192a863d7c7500c194918c323f48cb5
Victoria Sumner <vicsumner@gmail.com>
2019-01-29 10:06:28 -0700

    commit message

---
+++ exercises/morning-exercises/app.js
@@ -0,0 +1,44 @@
+/*
+    1. Write a function that takes a string as an parameter/argument and returns
+    that string uppercased.
+*/
+
+// function upperCaseMe(str) {
+//   var str = "good morning!";  
+//   console.log(str.toUpperCase());
+// }
+// upperCaseMe();
+
+// console.log(upperCaseMe("good morning!")) // GOOD MORNING!
+
+function upperCaseMe(str){
+  return str.toUpperCase();
+}
+upperCaseMe();
+
+/*
+    2. Write a function that takes a string and returns that string reversed.
+*/
+
+function reverseMe(str) {
+  var splitStr = "What do I spell backwards?";
+  console.log(str.reverse());
+}
+reverseMe();
+  // Your code here
+// }
+
+// console.log(reverseMe("What do I spell backwards?")) // ?sdrawkcab lleps I od tahW
+
+
+
+/*
+    3. Write a function that takes a string and a number.
+    Return the string slice from index 0 to that argument number.
+*/
+
+// function sliceMe(str, num) {
+  // Your code here
+// }
+
+// console.log(sliceMe("This is the string to slice.", 10)) // This is th
