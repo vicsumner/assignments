0f525ff038604515c2b0f9fe4352c0e01b86065b
Victoria Sumner <vicsumner@gmail.com>
2019-01-30 00:04:49 -0700

    commit message

--- exercises/ifStatement-Olympics/bronze-medal/bronze.js
+++ exercises/ifStatement-Olympics/bronze-medal/bronze.js
@@ -10,26 +10,26 @@
 // }
 
 // Ques. 2  ----------------------
-// var person = {
-//   name: "Bobby",
-//   age: 12
-// }
+var person = {
+  name: "Bobby",
+  age: 12
+}
 
-// if(person.name[0]==="B"){
-//   console.log(person.name + " is allowed to go to the movie.")
+if(person.name[0]==="B"){
+  console.log(person.name + " is allowed to go to the movie.")
 
-// }else{
-//   console.log(person.name + " cannot go to go the movie.")
-// }
+}else{
+  console.log(person.name + " cannot go to go the movie.")
+}
 
 // Ques. 3  ----------------------
 
-var person = {
-  name: "Bobby", 
-  age: 12
-}
-if(person.name[0]=== "B" && person.age >= 18){
-    console.log(person.name + "is allowed into the movie.");
-} else{
-  console.log(person.name + " is only " + person.age + ", so he can't go in.");
-}
+// var person = {
+//   name: "Bobby", 
+//   age: 12
+// }
+// if(person.name[0]=== "B" && person.age >= 18){
+//     console.log(person.name + "is allowed into the movie.");
+// } else{
+//   console.log(person.name + " is only " + person.age + ", so he can't go in.");
+// }
