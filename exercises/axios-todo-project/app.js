415d2c0804bd7e9cb4d64f92cbbffe6551bf08d8
Victoria Sumner <vicsumner@gmail.com>
2019-02-18 17:10:23 -0700

    commit message

--- exercises/axios-todo-project/app.js
+++ exercises/axios-todo-project/app.js
@@ -5,6 +5,7 @@ const todoForm = document.addToForm
 function getData(){
 axios.get('https://api.vschool.io/vicsumner/todo/').then(response => {
   const todos = response.data
+  // console.log(todos)
   listTodos(todos)
 
 }).catch(err => console.log(err))
@@ -12,47 +13,100 @@ axios.get('https://api.vschool.io/vicsumner/todo/').then(response => {
 
 function listTodos(todosArr){
   //Make 1 todo show up, then reuse that code to make all todos show up (unused direction)
-  
   // console.log(firstTodo)
-
-  for(let i =0 ; i < todosArr.length; i++){
-
+  for(let i = 0 ; i < todosArr.length; i++){
 //   //Make it show on the DOM
-//   // Create Elements
+//   // First Create Elements
       const todoContainer = document.createElement('div')
-      const title = document.createElement('h1')
-      const imgUrl = document.createElement('img')
+      
+      const checkbox = document.createElement('input')
+      const imgUrl = document.createElement('img') 
+      const deleteBtn = document.createElement('button')
+      const title = document.createElement('p')
+      const price = document.createElement('p')
+      const description = document.createElement('p')
+      
+      
 
-      //Edit the element / Give it content
+//2nd Edit the element / Give it content
       todoContainer.classList.add('todo-container')
+      
+      checkbox.checked =todosArr[i].completed
+      checkbox.type = 'checkbox'
+      imgUrl.setAttribute("src", todosArr[i].imgUrl) 
+      todoContainer["data-todoID"] = todosArr[i]._id
+      deleteBtn.textContent = "x"
       title.textContent = todosArr[i].title
-      imgUrl.setAttribute("src", todosArr[i].imgUrl)
-     
-      if(todosArr[i].completed){
-        title.style.textDecoration = "line-through"
-      }
+      price.textContent = todosArr[i].price
+      description.textContent = todosArr[i].description
+      
+      
+      
 
-    // Append it to the DOM
-      todoContainer.appendChild(title)
-      todoContainer.appendChild(imgUrl)
-      todoListContainer.appendChild(todoContainer)
+    checkbox.addEventListener("change", function (e) {
+      const ID = e.target.parentNode["data-todoID"]
+      const updates = { completed: e.target.checked }
+
+      axios.put(`https://api.vschool.io/vicsumner/todo/${ID}`, updates).then(response => { 
+        todoListContainer.innerHTML = ""
+        getData()
+       })
+    })
+
+    // delete each todo
+    deleteBtn.addEventListener("click", function(e){
+      const ID = e.target.parentNode["data-todoID"]
+      axios.delete(`https://api.vschool.io/vicsumner/todo/${ID}`).then(response =>{
+        e.target.parentNode.remove()
+      })
+    })
+
+      if (checkbox.checked) {
+        title.style.textDecoration = "line-through"
+        price.style.textDecoration = "line-through"
+        description.style.textDecoration = "line-through"
+      }
+  
+      
+// 3rd Append it to the DOM
+    
+    todoContainer.appendChild(checkbox)
+    todoContainer.appendChild(imgUrl)
+    todoContainer.appendChild(title)
+    todoContainer.appendChild(price)
+    todoContainer.appendChild(description) 
+    todoContainer.appendChild(deleteBtn)
+    todoListContainer.appendChild(todoContainer)
 
   }
-}
+} 
+
+    
+//submit event for form
 todoForm.addEventListener("submit", (e) => {
   e.preventDefault()
-    const newTodo = {
-      title: todoForm.title.value
 
-    }
+  const newTodo = {
+    title: todoForm.title.value,
+    price: todoForm.price.value,
+    description: todoForm.description.value,
+    imgUrl: todoForm.imgUrl.value
+  }
 
-    todoForm.title.value = ""
+  /// clear out data in input
+  todoForm.title.value = ""
+  todoForm.price.value = ""
+  todoForm.description.value = ""
+  todoForm.imgUrl.value = ""
+console.log(newTodo)
 
   axios.post('https://api.vschool.io/vicsumner/todo/', newTodo).then(response => {
     todoListContainer.innerHTML = ""
     getData()
-  }).catch(err => console.log(err))
+  })
+
 })
 
 getData()
 
+  
