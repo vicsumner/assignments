5e98d4571d318951ba933e8d52a470af7abb322a
Victoria Sumner <vicsumner@gmail.com>
2019-02-20 15:29:34 -0700

    first version of css zen garden

---
+++ exercises/props-intro/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react'
+// import myImg from './resources/img-test.jpeg'
+import List from './List.js'
+import './style.css'
+
+function App(props){
+  return(
+    <div>
+      <h1>Hello world</h1>
+      <img class="image" src=""/>
+      {/* <img class= "image" src ={myImg} /> */}
+      <List />
+    </div>
+  )
+}
+
+export default App
